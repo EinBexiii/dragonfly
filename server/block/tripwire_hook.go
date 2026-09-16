635def3fa17d50b32c477c81b8d887ea32240cab
EinBexiii <erikmiller04@gmail.com>
2026-09-16 17:58:23 +0200

    block: hang the tripwire hook on the wall it is placed against

--- server/block/tripwire_hook.go
+++ server/block/tripwire_hook.go
@@ -2,7 +2,9 @@ package block
 
 import (
 	"github.com/df-mc/dragonfly/server/block/cube"
+	"github.com/df-mc/dragonfly/server/item"
 	"github.com/df-mc/dragonfly/server/world"
+	"github.com/go-gl/mathgl/mgl64"
 )
 
 // TripwireHook is the block a tripwire is strung between. It is decoration here: no redstone logic is run.
@@ -16,6 +18,33 @@ type TripwireHook struct {
 	Attached bool
 	// Powered is whether the tripwire attached to the hook is being stepped on.
 	Powered bool
+}
+
+// UseOnBlock hangs the hook on the wall face clicked, pointing away from it.
+func (t TripwireHook) UseOnBlock(pos cube.Pos, face cube.Face, _ mgl64.Vec3, tx *world.Tx, user item.User, ctx *item.UseContext) bool {
+	pos, face, used := firstReplaceable(tx, pos, face, t)
+	if !used || face.Axis() == cube.Y {
+		return false
+	}
+	t.Facing = face.Direction()
+	if !t.supported(pos, tx) {
+		return false
+	}
+	place(tx, pos, t, user, ctx)
+	return placed(ctx)
+}
+
+// NeighbourUpdateTick drops the hook when its wall goes.
+func (t TripwireHook) NeighbourUpdateTick(pos, _ cube.Pos, tx *world.Tx) {
+	if !t.supported(pos, tx) {
+		breakBlock(t, pos, tx)
+	}
+}
+
+// supported reports whether the block behind the hook has a solid face to hang on.
+func (t TripwireHook) supported(pos cube.Pos, tx *world.Tx) bool {
+	wall := pos.Side(t.Facing.Face().Opposite())
+	return tx.Block(wall).Model().FaceSolid(wall, t.Facing.Face(), tx)
 }
 
 // BreakInfo ...
